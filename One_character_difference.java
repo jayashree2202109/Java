41edd5aad59cf04729f3c0e4b5c04eaad8e1eb40
jayashree2202109 <jayashree.2202109@srec.ac.in>
2025-12-04 11:09:46 +0530

    Initial code upload

---
+++ One_character_difference.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        if (s1.length() != s2.length()) {
+            System.out.println("no");
+            return;
+        }
+        int diffCount = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diffCount++;
+                if (diffCount > 1) break;
+            }
+        }
+        System.out.println(diffCount == 1 ? "yes" : "no");
+    }
+}
